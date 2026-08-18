0aa96c1e94d126bf72a3113e217b1bbb09fc8ff7
Perin0 <125603399+Perin0@users.noreply.github.com>
2026-08-18 23:34:59 +0000

    Criação do projeto

---
+++ ecommerce4tc-back/src/main/java/quatro/tc/ecommerce/ecommerce4tc_back/Ecommerce4tcBackApplication.java
@@ -0,0 +1,13 @@
+package quatro.tc.ecommerce.ecommerce4tc_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecommerce4tcBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ecommerce4tcBackApplication.class, args);
+	}
+
+}
---
+++ ecommerce4tc-back/src/test/java/quatro/tc/ecommerce/ecommerce4tc_back/Ecommerce4tcBackApplicationTests.java
@@ -0,0 +1,13 @@
+package quatro.tc.ecommerce.ecommerce4tc_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ecommerce4tcBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
